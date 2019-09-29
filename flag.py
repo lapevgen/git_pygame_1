dd721ba8cea23d95847c97117af30f728af09558
lapevgen <lapevgen@list.ru>
2019-09-29 23:13:57 +0300

    Флагшток

--- flag.py
+++ flag.py
@@ -1,9 +1,17 @@
 import pygame
 
 
+def draw_flag():
+    # рисуем флагшток
+    color = pygame.Color(255, 255, 20)
+    pygame.draw.rect(screen, color, (20, 20, 30, 420), 0)
+
+
 pygame.init()
 size = width, height = 800, 450  # размер окна
 screen = pygame.display.set_mode(size)
+
+draw_flag()
 
 pygame.display.flip()
 
